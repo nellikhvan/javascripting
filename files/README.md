a1e1ac5f1ee20a80d0a81cb410b8e43409b24998
Nelli Khvan <nelli.k@numeto.com>
2019-07-11 10:50:15 +0200

    Delete files dir

--- files/README.md
+++
@@ -1,3 +0,0 @@
-# Studying
-javascripting: https://github.com/workshopper/javascripting
-learnyounode: https://github.com/workshopper/learnyounode
--- files/program.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var fs = require('fs');
-
-
-console.log(process.argv[2]);
-fs.readdir(process.argv[2], (err, list) => {
-    console.log(list);
-});
