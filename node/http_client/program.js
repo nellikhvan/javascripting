4553a5d7f5145f661384476ed983c5243c89417e
Nelli Khvan <nelli.k@numeto.com>
2019-09-26 12:00:43 +0200

    Add http_client solution

---
+++ node/http_client/program.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var http = require('http');
+var url = process.argv[2];
+
+http.get(url, (response) => {
+    response.setEncoding('utf8');
+    response.on('data', console.log);
+    response.on('error', console.error);
+}).on('error', (e) => {
+    console.error(`Got error: ${e.message}`);
+});
